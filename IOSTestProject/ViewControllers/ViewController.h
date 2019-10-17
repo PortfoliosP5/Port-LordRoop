e4035f3fc6c15b324e3787a8795d7590a884e49a
Roop <jagroopvij@gmail.com>
2019-10-17 15:24:02 -0700

    almost done

--- IOSTestProject/ViewControllers/ViewController.h
+++ IOSTestProject/ViewControllers/ViewController.h
@@ -8,10 +8,30 @@
 
 #import <UIKit/UIKit.h>
 
+// Classic C style defines for Operator control
+#define PLUS 0
+#define MINUS 1
+#define MULTIPLY 2
+#define DIVIDE 3
+#define MODULO 4
+
 @interface ViewController : UIViewController
-{//calculation area display values
-    
+/* Section 1: Classic C style variables to support calculation */
+{
+    double arg1;
+    int mathOp;
+    double arg2;
+    double calcAnswer;
 }
+
+/* Section 2: Obj-C style variables to support Calc area management  */
+@property Boolean initialCalcAreaInputState;
+// Note: Obj-C helps with string memory management:  1. retention "strong"  2. multi-thread "nonatomic"
+@property (strong, nonatomic) NSString *calcAreaDefault;
+@property (strong, nonatomic) NSString *calcAreaNumber;
+
+/* Section 3: Obj-C style to support Interfacce Builder Outlet for Calc area */
+@property (strong, nonatomic) IBOutlet UILabel *calcAreaLabel;
 
 @end
 
--- IOSTestProject/ViewControllers/ViewController.m
+++ IOSTestProject/ViewControllers/ViewController.m
@@ -8,16 +8,132 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+//@interface ViewController ()
+//@end
 
-@end
+@implementation ViewController // beginning of implementation
 
-@implementation ViewController
+// syntesize enables getter and setter methods as used in Obj-C
+@synthesize initialCalcAreaInputState, calcAreaDefault, calcAreaNumber, calcAreaLabel;
 
-- (void)viewDidLoad {
+- (void)viewDidLoad {  // acts like a Constructor
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    /* Call to method to initialize calculator (set to zero) */
+    [self clearCalculator];
+    /* Default value for Calculator Area label */
+    [self setCalcAreaDefault:@"0.0"];
+    /* Call out to method to handle updating Calculator Area Label display */
+    [self clearCalcAreaLabel];
+}
+
+/* Section 1: Methods for managing and performing calculation, C style used where practical */
+-(void)calculateAnswer  // method to perform calculation
+{
+    // arg2 = [calcAreaNumber doubleValue];  // Obj-C Class method to convert NSSTRING to double
+    arg2 = calcAreaNumber.doubleValue;  // Alternate Java like syntax to convert NSSTRING to double
+    switch(mathOp)
+    {
+        case PLUS:
+            calcAnswer = arg1 + arg2;
+            break;
+        case MINUS:
+            calcAnswer = arg1 - arg2;
+            break;
+        case DIVIDE:
+            calcAnswer = arg1 / arg2;
+            break;
+        case MULTIPLY:
+            calcAnswer = arg1 * arg2;
+            break;
+        case MODULO:
+            calcAnswer = (double)((int)arg1 % (int)arg2);
+            break;
+        case -1:
+            calcAnswer = arg1;
+    }
+}
+
+-(void)saveValueOfArg1 { // method to store 1st value in calculation (arg1), C style
+    // arg1 = [calcAreaNumber doubleValue];  // Obj-C Class method to convert NSSTRING to double
+    arg1 = calcAreaNumber.doubleValue;  // Alternate Java like syntax to convert NSSTRING to double
+}
+
+-(void)saveValueofAnswer {  // method to save value of answer after calc to arg1
+    mathOp = -1;          // operator is unassigned after calc
+    arg1 = calcAnswer;      // arg1 is current display value
+    arg2 = 0.0;             // arg2 is now unassigned
+}
+
+-(void)saveValueOfOperator:(int)opNumber {  // method to retain value of operator
+    mathOp = opNumber;
+}
+
+-(void)clearCalculator {                    // method to clear values of calculator
+    mathOp = -1;
+    arg1 = 0.0;
+    arg2 = 0.0;
+    calcAnswer = 0.0;
+}
+/* End Section 1 */
+
+/* Section 2:  View Control helper methods to manage Calculation Area */
+-(void)setTextCalcAreaLabel {  // helper method to set and thus update text in Calc Area
+    [calcAreaLabel setText:calcAreaNumber];
+}
+
+-(void)concatCalcAreaLabel:(NSString *)keyNumber {  // helper method for handling number being input
+    if (initialCalcAreaInputState) {  // sets number vs concat on initial key typed
+        [self setCalcAreaNumber:keyNumber];
+        [self setInitialCalcAreaInputState:false];
+    } else  {                         // concats number ot end on subsequent keys typed
+        [self setCalcAreaNumber:[calcAreaNumber stringByAppendingString:keyNumber]];
+    }
+    [self setTextCalcAreaLabel];
+}
+
+-(void)clearCalcAreaLabel {  // helper method to clear and update text in Calc Area to default
+    [self setCalcAreaNumber:calcAreaDefault];
+    [self setInitialCalcAreaInputState:true];  // set calc area state to starting state
+    [self setTextCalcAreaLabel];
+}
+/* End Section 2 */
+
+
+// Section 3: Interface Builder actions for buttons
+-(IBAction)equalButton:(id)sender {  // Interface Builder action for equal (calculation)
+    // perform calculation
+    [self calculateAnswer];
+    
+    // set and display result
+    [self setCalcAreaNumber:[NSString stringWithFormat:@"%f", calcAnswer]];  // float (double) to string
+    [self setTextCalcAreaLabel];
+    
+    // set values to support continued calculations, but wipe if you type a number
+    [self saveValueofAnswer];                   // answer -> arg1
+    [self setInitialCalcAreaInputState:true];   // number key typing will wipe value
+}
+
+-(IBAction)clearButton:(id)sender { // Interface Builder action for clear (clean calculator)
+    [self clearCalculator];          // clear operator
+    [self clearCalcAreaLabel];
+}
+
+// Interface Builder for operators buttons
+-(IBAction)plusButton:(id)sender {  // Interface Builder action for plus (+)
+    [self saveValueOfOperator:PLUS];
+    [self saveValueOfArg1];
+    [self clearCalcAreaLabel];
 }
 
 
-@end
+// Interface Builder actions  for numbers and decimal
+-(IBAction)press9Button:(id)sender {  // Interface Builder action for (9)
+    NSString *keyNumber = @"9";
+    [self concatCalcAreaLabel:keyNumber];
+}
+
+
+/* End Section 2 */
+
+
+@end  // End of Implementation
